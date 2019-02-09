9fe2a42195bc6b463a2b46061cfb347d76e3114c
Ying Wen <wenyingying@hotmail.com>
2019-02-08 23:11:25 -0500

    Add mapReduce.py

---
+++ mapReduce.py
@@ -0,0 +1,19 @@
+from mrjob.job import mrjob
+from mrjob.step import MRStep
+
+class RatingsBreakdown(MRjob):
+    def steps(self):
+        return [
+        MRStep(mapper=self.mapper_get_ratings,
+                reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp)= line.split('\t')
+        yield rating, 1
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(value)
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
